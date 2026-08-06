50cbda401fd23780f5986b03877e6080695137e8
frogle-dev <jonah0602@outlook.com>
2026-08-06 07:47:01 -0700

    split app.hpp into app.hpp and app.cpp

--- 02_cube_viewer/run.sh
+++ 02_cube_viewer/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ./build.sh
-VK_LAYER_VALIDATE_SYNC=1 ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer) ASAN_OPTIONS=symbolize=1 LSAN_OPTIONS=suppressions=lsan_suppressions.txt ./build/learn_vulkan
+VK_LAYER_VALIDATE_SYNC=1 ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer) ASAN_OPTIONS=symbolize=1 LSAN_OPTIONS=suppressions=LSAN.supp ./build/learn_vulkan
